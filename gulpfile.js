0c3535286c4313b5870235ffddf3c32645eb15d6
Dan Schapira <dsschapira@gmail.com>
2018-08-03 16:02:41 -0400

    Update gulp files and calls to css and js files form index

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var minHTML = require('gulp-htmlmin');
+var order = require('gulp-order');
 
 gulp.task('styles', function(){
     gulp.src('css/**/*.css')
@@ -13,7 +14,33 @@ gulp.task('styles', function(){
 });
 
 gulp.task('scripts', function(){
-    gulp.src('js/**/*.js')
+    // gulp.src('js/**/*.js')
+    //     .pipe(order([
+    //         'js/jquery.js',
+    //         'js/ajaxchimp.js',
+    //         'js/owl.carousel.min.js',
+    //         'js/wow.js',
+    //         'js/parallax.js',
+    //         'js/nicescroll.js',
+    //         'js/main.js',
+    //         'js/scrollTo.js',
+    //         'js/**/*.js'
+    //     ]))
+    gulp.src('js/jquery.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/ajaxchimp.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/owl.carousel.min.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/wow.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/parallax.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/nicescroll.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/main.js')
+        .pipe(concat('logic.js'));
+    gulp.src('js/scrollTo.js')
         .pipe(concat('logic.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
@@ -21,7 +48,7 @@ gulp.task('scripts', function(){
 
 gulp.task('copy-html', function(){
     gulp.src('./index.html')
-        .pipe(minHTML({collapsedWhitespace: true}))
+        .pipe(minHTML({collapseWhitespace: true, caseSensitive: true}))
         .pipe(gulp.dest('dist'));
 });
 
